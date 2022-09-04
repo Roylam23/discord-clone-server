38d38a784f5bec688aa80979338cf511b898e653
Roylam23 <roylam23@gmail.com>
2022-09-04 11:15:09 +0800

    v0.0.1

--- index.ts
+++ index.ts
@@ -3,14 +3,15 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const helmet = require("helmet");
 
-app.use(cors());
+app.use(cors(), helmet());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
