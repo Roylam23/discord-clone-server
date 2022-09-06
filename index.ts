61938564578f8e0e1f4c60b5a082592e4650b2ea
Roylam23 <roylam23@gmail.com>
2022-09-06 11:03:50 +0800

    v0.0.2e

--- index.ts
+++ index.ts
@@ -19,8 +19,19 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
   socket.on("join_room", (data) => {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
     socket.join(data);
-    socket.emit("return_username", socket.id);
+    socket.emit("return_entrydata", {
+      id: socket.id,
+      year: year,
+      month: month,
+      date: date,
+    });
   });
 
   socket.on("leave_room", (data) => {
@@ -29,34 +40,25 @@ io.on("connection", (socket) => {
   });
   socket.on("send_message", (data) => {
     let date_ob = new Date();
-
-    // current date
-    // adjust 0 before single digit date
     let date = ("0" + date_ob.getDate()).slice(-2);
-
-    // current month
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-    // current year
     let year = date_ob.getFullYear();
-
-    // current hours
     let hours = date_ob.getHours();
-
-    // current minutes
     let minutes = date_ob.getMinutes();
     const str_minutes = minutes < 10 ? `0${minutes}` : minutes;
-    // current seconds
     let seconds = date_ob.getSeconds();
+    // console.log(data.message);
     socket.to(data.room).emit("receive_message", {
       data: data,
       time: hours + ":" + str_minutes,
       user: socket.id,
+      date: { year, month, date },
     });
     socket.emit("receive_message", {
       data: data,
       time: hours + ":" + str_minutes,
       user: socket.id,
+      date: { year, month, date },
     });
   });
 });
